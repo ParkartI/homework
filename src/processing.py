bb7fdaeee33f618493fffcb01db46e4019cb96eb
Artur <tisifon84@gmail.com>
2024-11-14 21:04:30 +0500

    entering test files

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,12 @@
 from typing import List, Dict
 
 
-def filter_by_state(data: dict, state='EXECUTED'): -> dict
-    """Функция возвращает словари у которых ключ state соответствует указанному значению"""
-
+def filter_by_state(data: dict, state: str ='EXECUTED') -> list:
+    """Возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению"""
     return [entry for entry in data if entry.get('state') == state]
 
 
 def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+    """Возвращаtn новый список, отсортированный по дате"""
     return sorted(data, key=lambda x: x['date'], reverse=descending)
