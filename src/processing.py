e8095ff124050bc4cd9845a3f70f4d2186f7b934
Artur <tisifon84@gmail.com>
2024-11-17 19:26:11 +0500

    fix error

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-from typing import Dict, List
+from typing import List, Dict
 
 
-def filter_by_state(data: List[Dict], state="EXECUTED") -> List[Dict]:
-    """Функция возвращает список словарей, у которых ключ 'state' соответствует указанному значению"""
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция возвращает список словарей, у которых ключ 'state' соответствует указанному значению."""
     return [entry for entry in data if entry.get("state") == state]
 
 
 def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
-    """Сортирует список словарей по ключу 'date'"""
-    return sorted(data, key=lambda x: x["date"], reverse=descending)
+    """Сортирует список словарей по ключу 'date'."""
+    return sorted(data, key=lambda entry: entry["date"], reverse=descending)
